1c08fe5c7d8e126a7ed49bb4d997f22ce187f03f
Franck Pommereau <franck.pommereau@gmail.com>
2024-09-30 14:20:46 +0200

    added precompiled libITS

--- setup.py
+++ setup.py
@@ -48,24 +48,9 @@ if DDDPATH is None:
     sys.stderr.write("*** could not find libddd ***")
     sys.exit(1)
 
-DDDLIB = DDDPATH.parent
-BUILD = Path("build")
-ITSURL = "https://lip6.github.io/libITS/linux.tgz"
-ITSTGZ = BUILD / "libITS.tgz"
-ITSINC = BUILD / "usr/local/include"
-ITSLIB = BUILD / "usr/local/lib"
+ITSLIB = DDDPATH.parent
+ITSINC = ITSLIB.parent / "include"
 os.environ["CXX"] = "g++"
-
-BUILD.mkdir(exist_ok=True)
-if not Path(ITSTGZ).exists():
-    print(f"downloading {ITSURL!r}")
-    with urllib.request.urlopen(ITSURL) as remote, open(ITSTGZ, "wb") as local:
-        local.write(remote.read())
-
-with tarfile.open(ITSTGZ) as tar:
-    tar.extractall(BUILD)
-for path in (BUILD / "usr/local/lib").glob("libDDD.*"):
-    path.unlink()
 
 long_description = Path("README.md").read_text(encoding="utf-8")
 description = (long_description.splitlines())[0]
@@ -94,7 +79,7 @@ setup(
                 include_dirs=[str(ITSINC)],
                 libraries=["DDD", "antlr3c", "expat", "gmp", "gmpxx"],
                 extra_objects=[str(ITSLIB / "libITS.a")],
-                library_dirs=[str(ITSLIB), str(DDDLIB)],
+                library_dirs=[str(ITSLIB)],
                 extra_compile_args=["-std=c++11"],
                 extra_link_args=["-Wl,--no-as-needed"],
             )
